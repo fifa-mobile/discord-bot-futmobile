7aaf6e2310f4921f9e2bb637e96705704b7a49cc
yuulye <yuulye@gmail.com>
2019-12-15 09:29:32 +0000

    fixtures + left, played

--- tour-fixtures-data.js
+++ tour-fixtures-data.js
@@ -13,15 +13,17 @@ module.exports = (args, msg, sheet, players) => {
       let cell = cells[i];
       let score = cell.value.split('-');
       score = score.map(s => s.trim());
+      let isPlayed = score[0].length;
       let match =
         (i + 1).toString().padStart(2, ' ') + '| '
-        + players[cell.row - 2].padEnd(16, ' ')
-        + score[0].padStart(3, ' ')
-        + ' vs '
-        + score[1].padEnd(3, ' ')
-        + players[cell.col - 2].padStart(16, ' ')
-      //,`r#${cell.row}c#${cell.col}: ${cell.value}`
+        + players[cell.row - 2].padEnd(14, ' ')
+        + score[0].padStart(2, ' ')
+        + ' - '
+        + score[1].padEnd(2, ' ')
+        + players[cell.col - 2].padStart(14, ' ')
       ;
+      if (args[1] === 'played' && !isPlayed) continue;
+      if (args[1] === 'left' && isPlayed) continue;
       o += match + "\n";
     }
     console.log(o);
