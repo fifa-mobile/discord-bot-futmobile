6a2cbbafb56c72a323bbe340b0341ba9179b6d0a
juve938383 <58861443+juve938383@users.noreply.github.com>
2020-02-01 15:06:05 +0100

    Added $ping and $help

--- bot.js
+++ bot.js
@@ -53,4 +53,37 @@ module.exports = (u, msg, cmd, args, client) => {
       require('./bot/league.js')(u, msg, args)
     );
   }
+  if (cmd === 'ping'){
+    const pingTypes = [
+  "🏓|**Pong.**Testing,testing.Is this thing working?",
+  "🏓|**Pong.**Match point.Let's see what you've got.",
+  "🏓|**Pong.**Keep on pinging me.",
+  "🏓|**Pong.**I am alive.",
+  "🏓|**Pong.**Can't play now.Got a meeting with the bots.We're planning to conquer the wor... Wait!Did I say that loudly?!",
+  "🏓|**Pong.**You had a 0.1% chance of getting this message.",
+];
+const pingMultiply = [
+  250,
+  250,
+  250,
+  244,
+  5,
+  1,
+];
+
+ let pingNames = [];
+for (let i = 0; i < pingTypes.length; i++) {
+  const type = pingTypes[i];
+  for (let j = 0; j < pingMultiply[i]; j++) {
+    pingNames.push(type);
+  }
+}
+
+ let pongName = pingNames[Math.floor(Math.random()* pingNames.length)];
+var pong=pongName;
+m.channel.send(pong);
+}
+   if(cmd==='help'){
+m.channel.send(" `Standard Command List` \n\n  **1.Info.**-`help` `ping` ;\n **2.League**-`league`; );
+}
 };
