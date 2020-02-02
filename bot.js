9d1af0e72bed7e4e8edaf2494ce8ab8fd8aa5deb
juve938383 <58861443+juve938383@users.noreply.github.com>
2020-02-02 19:23:00 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -81,7 +81,7 @@ var pong=pongName;
 msg.channel.send(pong);
 }
    if(!args[0] && cmd==='help'){
-msg.channel.send(" `Standard Command List \n` \n\n  **1.Info.**-`help` `ping` ;\n **2.League**-`league` `tour`; \n **3.Fun**- `read`;");
+msg.channel.send(" `Standard Command List          ` \n\n  **1.Info.**-`help` `ping` ;\n **2.League**-`league` `tour`; \n **3.Fun**- `read`;");
 }
    if(cmd==='help' && args[0] && args[0] === 'help'){
 msg.channel.send("**`$help`** \n **Use**:Used to show a list of commands.Also shows command specific help. \n **Command**:$help [command] \n\n **Example**:\n `$help`-Shows command list. \n `$help league`-Explains the use of the `league` command.");
