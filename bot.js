d9adcd1dd0ff6b2dd5871381541fd6e012035078
juve938383 <58861443+juve938383@users.noreply.github.com>
2020-02-03 15:01:18 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -84,7 +84,7 @@ msg.channel.send(pong);
 msg.channel.send(" ```Standard Command List```\nℹ | Use `$help [command]` if you want to get more info on a specific command. \n\n  **1.Info.**-`help` `ping` `bot` ;\n **2.League**-`league` `tour`; \n **3.Fun**- `read`;\n\n ```>Don't include brackets when using the commands.```");
 }
    if(cmd==='help' && args[0] && args[0] === 'help'){
-msg.channel.send("**```$help```** \n **Use**:Used to show a list of commands.Also shows command specific help. \n **Command**:$help [command] \n\n **Example**:\n `$help`-Shows command list. \n `$help league`-Explains the use of the `league` command.\n\n ```>Don't include brackets when using the commands.```");
+msg.channel.send("**```$help```** \n **Use**:Used to show a list of commands.Also shows command specific help. \n **Command**:$help `[command]` \n\n **Example**:\n `$help`-Shows command list. \n `$help league`-Explains the use of the `league` command.\n\n ```>Don't include brackets when using the commands.```");
 }
    if(cmd==='help'&& args[0] && args[0] === 'ping' ){
 msg.channel.send("**```$ping```** \n **Use**:Used to check if bot is running. \n **Command**:$ping \n\n **Example**:\n `$ping`-Bot answers if everything is good.\n\n ```>Don't include brackets when using the commands.```");
