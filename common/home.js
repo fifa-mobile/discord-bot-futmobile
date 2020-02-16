0aaed97f0102f8322555fbbafbbb5b444b847e32
yuulye <yuulye@gmail.com>
2020-02-16 08:31:35 +0700

    prevent app crashed

--- common/home.js
+++ common/home.js
@@ -1,32 +1,6 @@
 module.exports = (u) => {
   const http = require('http');
 
-  /*
-  const router = require('./router');
-
-  router.register('/', function(req, res) {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World!\n');
-  });
-
-  router.register('/data', function(req, res) {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    require('./sheets.js')(
-      require('../data/ahq-tournament-data.js')(u, res)
-      , '19HSTRRyScAtnzyYcXCFz_9rzopum5jLi7dQ5YpAgl8k'
-    );
-  });
-
-  router.register('/hi', function(req, res) {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hi!\n');
-  });
-  */
-
   const server = http.createServer(function (req, res) {
     const url = req.url;
     if (url === '/') {
@@ -37,14 +11,6 @@ module.exports = (u) => {
       res.statusCode = 200;
       res.setHeader('Content-Type', 'text/plain');
       res.end('Hi!\n');
-    } else if (url === '/data') {
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json');
-      res.setHeader("Access-Control-Allow-Origin", "*");
-      require('./sheets.js')(
-        require('../data/ahq-tournament-data.js')(u, res)
-        , '19HSTRRyScAtnzyYcXCFz_9rzopum5jLi7dQ5YpAgl8k'
-      );
     } else {
       res.statusCode = 404;
       res.end('404, sorry!\n');
